1e9a872b1053c258c6dad89c4a1582195711400a
Bill Fischofer <bill.fischofer@linaro.org>
2017-03-04 16:42:29 +0100

    linux-generic: packet: restructure inline routines to use macros
    Use an internal macro for accessing inline fields in preparation for
    introducing packet reference support.
    
    Signed-off-by: Bill Fischofer <bill.fischofer@linaro.org>

--- platform/linux-generic/include/odp/api/plat/packet_flag_inlines.h
+++ platform/linux-generic/include/odp/api/plat/packet_flag_inlines.h
@@ -16,14 +16,17 @@
 #include <odp/api/plat/packet_types.h>
 #include <odp/api/hints.h>
 
+/** @internal Packet field accessor */
+#define _odp_pkt_get(pkt, cast, field) \
+	(*(cast *)(uintptr_t)((uint8_t *)pkt + _odp_packet_inline.field))
+
 /** @internal Inline function offsets */
 extern const _odp_packet_inline_offset_t _odp_packet_inline;
 
 /** @internal Inline function @param pkt @return */
 static inline uint64_t _odp_packet_input_flags(odp_packet_t pkt)
 {
-	return *(uint64_t *)(uintptr_t)((uint8_t *)pkt +
-	       _odp_packet_inline.input_flags);
+	return _odp_pkt_get(pkt, uint64_t, input_flags);
 }
 
 /** @internal Inline function @param pkt @return */
