c67857bec7af30b2f0607d60814e7dc2310411fc
Maxim Uvarov <maxim.uvarov@linaro.org>
2018-06-15 22:41:17 +0300

    linux-gen: armv7: fix isb usage
    correct usage of arm isb instruction in inline assembly code
          instruction requires: data-barriers
    asm volatile("isb" ::: "memory");
    <inline asm>:1:2:
    nstantiated into assembly here
    
    Signed-off-by: Maxim Uvarov <maxim.uvarov@linaro.org>

--- platform/linux-generic/arch/arm/odp/api/abi/cpu.h
+++ platform/linux-generic/arch/arm/odp/api/abi/cpu.h
@@ -20,7 +20,7 @@ static inline void odp_cpu_pause(void)
 	 * ISB flushes the pipeline, then restarts. This is guaranteed to
 	 * stall the CPU a number of cycles.
 	 */
-	__asm volatile("isb" ::: "memory");
+	__asm volatile("isb sy" ::: "memory");
 }
 
 #ifdef __cplusplus
