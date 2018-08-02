a5f391a71ec876b39cfd2a13b427b8895b4a727a
Maxim Uvarov <maxim.uvarov@linaro.org>
2018-08-02 17:45:50 +0300

    ci: refine coverage tests
    Signed-off-by: Maxim Uvarov <maxim.uvarov@linaro.org>

--- scripts/ci/coverage.sh
+++ scripts/ci/coverage.sh
@@ -10,21 +10,47 @@ CWD=$(dirname "$0")
 TDIR=`mktemp -d -p ~`
 
 cd ${TDIR}
+echo 1000 | tee /proc/sys/vm/nr_hugepages
+mkdir -p /mnt/huge
+mount -t hugetlbfs nodev /mnt/huge
 
 git clone ${CWD}/../../ odp
 cd ./odp
 ./bootstrap
 ./configure \
-	CFLAGS="-O0 -coverage" CXXFLAGS="-O0 -coverage" LDFLAGS="--coverage" \
+	CFLAGS="-O0 -coverage $CLFAGS" CXXFLAGS="-O0 -coverage $CXXFLAGS" LDFLAGS="--coverage $LDFLAGS" \
 	--enable-debug=full --enable-helper-linux --enable-dpdk
 export CCACHE_DISABLE=1
 make -j $(nproc)
 
+# ignore possible failures there because these tests depends on measurements
+# and systems might differ in performance.
+export ci="true"
+
 ODP_SCHEDULER=basic    make check
+if [ $? -ne 0 ]; then
+  find . -name "*.trs" | xargs grep -l '^.test-result. FAIL' | while read trs ; do echo FAILURE detected at $trs; cat ${trs%%.trs}.log ; done
+fi
+
 ODP_SCHEDULER=sp       make check
+if [ $? -ne 0 ]; then
+  find . -name "*.trs" | xargs grep -l '^.test-result. FAIL' | while read trs ; do echo FAILURE detected at $trs; cat ${trs%%.trs}.log ; done
+fi
+
 ODP_SCHEDULER=iquery   make check
+if [ $? -ne 0 ]; then
+  find . -name "*.trs" | xargs grep -l '^.test-result. FAIL' | while read trs ; do echo FAILURE detected at $trs; cat ${trs%%.trs}.log ; done
+fi
+
 ODP_SCHEDULER=scalable make check
+if [ $? -ne 0 ]; then
+  find . -name "*.trs" | xargs grep -l '^.test-result. FAIL' | while read trs ; do echo FAILURE detected at $trs; cat ${trs%%.trs}.log ; done
+fi
+
+
 bash <(curl -s https://codecov.io/bash) -X coveragepy
 
 cd ~
 rm -rf ${TDIR}
+
+umount /mnt/huge
