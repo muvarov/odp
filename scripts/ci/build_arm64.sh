34d6efbb10c1117dfb1af7e45153f880a0fcdbe5
Maxim Uvarov <maxim.uvarov@linaro.org>
2018-08-17 23:53:02 +0300

    try to fix arm64
    Signed-off-by: Maxim Uvarov <maxim.uvarov@linaro.org>

--- scripts/ci/build_arm64.sh
+++ scripts/ci/build_arm64.sh
@@ -15,21 +15,35 @@ fi
 export PKG_CONFIG_PATH=/usr/lib/aarch64-linux-gnu/pkgconfig:/usr/aarch64-linux-gnu/pkgconfig
 export PKG_CONFIG_PATH="$HOME/cunit-install/aarch64/lib/pkgconfig:${PKG_CONFIG_PATH}"
 
+CWD=$(dirname "$0")
+TDIR=`mktemp -d -p ~`
+
+cd ${TDIR}
+
+git clone ${CWD}/../../ odp
+cd ./odp
+
 cd ~
 export CROSS_ARCH=arm64
 export DPDK_CROSS=aarch64-linux-gnu-
 export TARGET="arm64$DPDKCC"
 
 dpkg -i --force-depends ~/download/libpcap0.8-dev_1.5.3-2_arm64.deb
-./build_dpdk.sh
+
+git clone ${CWD}/../../ odp
+cd ./odp
+./scripts/ci/build_dpdk.sh
 DPDKPATH=`cat /tmp/dpdk_install_dir`
 
-git clone /odp
-cd ./odp
 ./bootstrap
 ./configure --host=aarch64-linux-gnu --build=x86_64-linux-gnu --with-dpdk-path=${DPDKPATH} \
 	--disable-test-cpp ${CONF}
 make clean
 make -j 8
-cd ..
-rm -rf odp
+
+if [ $? -ne 0 ]; then
+  find . -name "*.trs" | xargs grep -l '^.test-result. FAIL' | while read trs ; do echo FAILURE detected at $trs; cat ${trs%%.trs}.log ; done
+fi
+
+cd ~ 
+rm -rf ${TDIR}
